e1b9f04c4393bece5ec6b0c7b8a7b93ff4512ec4
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-02-19 16:56:24 +0330

    Feat: context and get data

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import ProductsPage from "./pages/ProductsPage"
 import NotFoundPage from "./pages/NotFoundPage"
 import DetailsPage from "./pages/DetailsPage"
 import CheckoutPage from "./pages/CheckoutPage"
+//context
+import ProductsProvider from "./context/ProductsProvider";
 
 function App() {
   return (
-    <Routes>
-      <Route index path="/" element={<Navigate to="/products"  replace/>} />
-      <Route path="/products" element={<ProductsPage/>} />
-      <Route path="/products/:id" element={<DetailsPage/>} />
-      <Route path="/checkout" element={<CheckoutPage/>} />
-      <Route path="/*" element={<NotFoundPage/>} />
-    </Routes>
+    <ProductsProvider>
+      <Routes>
+        <Route index path="/" element={<Navigate to="/products"  replace/>} />
+        <Route path="/products" element={<ProductsPage/>} />
+        <Route path="/products/:id" element={<DetailsPage/>} />
+        <Route path="/checkout" element={<CheckoutPage/>} />
+        <Route path="/*" element={<NotFoundPage/>} />
+        </Routes>
+    </ProductsProvider>
   )
 }
 
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
+import { useProducts } from '../context/ProductsProvider'
+
 function ProductsPage() {
+  const products = useProducts()
+  console.log(products)
   return (
     <div>Products</div>
   )
