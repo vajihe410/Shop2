53fa4e0f6c01690fdb94a3b042d9a195941c4817
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-03-11 15:24:50 +0330

    Feat:filter & category products & handle search query

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,4 +1,39 @@
 const shorten = (text) => {
     return text.split(" ").slice(0,3).join(" ")
 }
-export {shorten}
+const searchProducts = (products,search) => {
+    if (!search) return products
+    const searchedProducts =products.filter(product => product.title.toLowerCase().includes(search))
+    return searchedProducts
+  }
+
+const filterProducts = (products,category) => {
+    if(!category) return products
+    
+    const filterdProducts = products.filter(product => (product.category === category))
+    return filterdProducts
+
+}
+const createQueryObject = (currentQuery , newQuery) => {
+  if(newQuery.category === "all"){
+    const {category, ...rest} = currentQuery
+    return rest
+
+  }
+  if(newQuery.search === ""){
+    const {search, ...rest} = currentQuery
+    return rest
+  }
+  return {...currentQuery , ...newQuery,}
+ 
+}
+const getInitialQuery = (searchParams) => {
+  const query = {}
+  const category = searchParams.get("category")
+  const search = searchParams.get("search")
+  if(category) query.category = category
+  if(search) query.search = search 
+  return query
+}
+ 
+export {shorten ,searchProducts ,filterProducts, createQueryObject ,getInitialQuery}
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 //context
 import { useProducts } from '../context/ProductsProvider'
 //styles
@@ -6,17 +7,74 @@ import styles from './ProductsPage.module.css'
 //componets
 import Card from '../components/Card'
 import Loader from '../components/Loader'
+//icons
+import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
+//functions
+import { createQueryObject, filterProducts, searchProducts, getInitialQuery } from '../helpers/helper'
 
 function ProductsPage() {
   const products = useProducts()
+
+  const [ search , setSearch ] = useState("")
+  const [ showProducts , setShowProducts ] = useState([])
+  const [ query , setQuery ] = useState({})
+  const [ searchParams , setSearchParams ] = useSearchParams()
+
+  useEffect(() => {
+    setShowProducts(products)
+    setQuery(getInitialQuery(searchParams))
+  },[products])
+
+  useEffect(()=>{
+    setSearchParams(query)
+    setSearch(query.search || "")
+    let finalProducts = searchProducts(products,query.search)
+    finalProducts = filterProducts(finalProducts,query.category)
+
+    setShowProducts(finalProducts)
+ 
+  },[query])
+
+  const SearchHandler = () => {
+    setQuery(query => (createQueryObject(query,{search})))
+  }
+  const categoriesHandler = (event) => {
+    
+    const {tagName} = event.target
+    const category = event.target.innerText.toLowerCase()
+  
+    if(tagName !== "LI") return
+
+    setQuery(query => (createQueryObject(query,{category})))
+  }
   return (
-    <div className={styles.container}>
-      {!products.length && <Loader/> }
-      <div className={styles.products}>
-        {products.map(product =><Card key={product.id} product={product}/> )}
+    <>
+      <div>
+        <input placeholder='Search...' type='text' value={search} onChange={(event) => setSearch(event.target.value.toLowerCase().trim())}/>
+        <button onClick={SearchHandler}><ImSearch /></button>
       </div>
-      <div className={styles.sidebar}>Sidebar</div>
+      <div className={styles.container}>
+      {!showProducts.length && <Loader/> }
+      <div className={styles.products}>
+        {showProducts.map(product =><Card key={product.id} product={product}/> )}
+      </div>
+      <div className={styles.sidebar}>
+      <div>
+        <FaListUl/>
+        <p>Categories</p>
+      </div>
+      <ul onClick={categoriesHandler}>
+        <li>All</li>
+        <li>Electronics</li>
+        <li>Jewelery</li>
+        <li>Men's Clothing</li>
+        <li>Woman's Clothong</li>
+      </ul>
+      </div>
     </div>
+    </>
+
   )
 }
 
